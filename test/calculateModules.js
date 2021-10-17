47aa868ebd10d88b40f392a1e94fb0feecd54d67
BryanMerlos <bryan.merlosd@gmail.com>
2021-10-16 21:48:21 -0500

    adding functionality to calculateModules

--- test/calculateModules.js
+++ test/calculateModules.js
@@ -1,1 +1,11 @@
 //**d)Write a program to calculate the modules of two numbers without using any inbuilt modulus operator */
+
+function calculateModule(num, divisor) {
+    while (num >= divisor)
+        num -= divisor;
+    return num;
+}
+ 
+// Driver code
+let num = 10, divisor = 7;
+console.log(calculateModule(num, divisor));
