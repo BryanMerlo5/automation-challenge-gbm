a8795e1b8fa50a7ff57e1d8eddaabcfbe92fc843
BryanMerlos <bryan.merlosd@gmail.com>
2021-10-16 21:48:44 -0500

    adding functionality to numberDaysByMonth

--- test/numberDaysByMonth.js
+++ test/numberDaysByMonth.js
@@ -1,1 +1,9 @@
 //**e)Write a program to find the number of days in a month*/
+function daysInMonth() {
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    console.log("Number of days in " + month);
+
+}
+
+daysInMonth();
