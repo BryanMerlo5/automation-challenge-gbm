7bb6fb822754da248b9acca97312718d606d56ee
BryanMerlos <bryan.merlosd@gmail.com>
2021-10-16 21:49:03 -0500

    adding functionality to validateArray

--- test/validateArray.js
+++ test/validateArray.js
@@ -1,1 +1,15 @@
 /**b)From the following array ['a''b''c''d'], validate if it contains the following values: 'a' 'b' 'f'. Return a Boolean depending on the result*/
+
+const arr = ['a', 'b', 'c', 'd'];
+
+function isPresentInArray(arr, val) {
+  let value = arr.some(arrVal => val === arrVal);
+  if(value){
+    console.log('The value ' + val + ' is present in the array: ', arr);
+  } else {
+    console.log('The value ' + val + ' is not present in the array: ', arr);
+  }
+}
+
+isPresentInArray(arr, 'x');
+isPresentInArray(arr, 'a');
