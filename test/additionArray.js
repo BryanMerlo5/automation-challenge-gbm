8c3d3f7ff3ae621e60826871540951f075ef4b57
BryanMerlos <bryan.merlosd@gmail.com>
2021-10-17 13:31:15 -0500

    adding sum functionality

--- test/additionArray.js
+++ test/additionArray.js
@@ -1,1 +1,14 @@
 //**c)Sum all the elements of an array: [1,34,56,72,123]*/
+
+function sumValuesInArray(arr){
+    let total = 0;
+    let i = 0;
+    for(i in arr) { 
+        total += arr[i];
+    }
+    return total;
+}
+
+let arr = [1, 34, 56, 72, 123];
+
+console.log('The total sum is: ', sumValuesInArray(arr));
