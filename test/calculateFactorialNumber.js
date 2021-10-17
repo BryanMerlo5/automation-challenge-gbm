4c55b2ff95f1b88fff4d6dc83166e54a8230cd7c
BryanMerlos <bryan.merlosd@gmail.com>
2021-10-17 13:21:06 -0500

    adding calculateFactirialNumber

--- test/calculateFactorialNumber.js
+++ test/calculateFactorialNumber.js
@@ -1,8 +1,17 @@
 /**a)Calculate the factorial of a number */
 
+function calculateFactorial(num) {
+    let factorial = num;
+    if (num === 0 || num === 1) 
+      return 1; 
+    while (num > 1) { 
+      num--;
+      factorial *= num;
+    }
+    return factorial;
+}
 
-
-
+console.log('The factorial is: ', calculateFactorial(5));
 
 
 
