6002fda1d497744e5c7992dde5e2e5cac4afcb3c
BryanMerlos <bryan.merlosd@gmail.com>
2021-10-18 14:28:00 -0500

    last change

--- src/pageobjects/page.js
+++ src/pageobjects/page.js
@@ -2,13 +2,12 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
+import openWebsite from '../utils/openWebsite';
+
 export default class Page {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    openWebsite (type, page) {
-        const url = (type === 'url') ? page : browser.options.baseUrl + page;
-        return browser.url(url)
-    }
+    openWebsite
 }
